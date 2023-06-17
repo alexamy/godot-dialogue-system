7e5c06afa74bca68e3365facfa0e6833441bd5d3
Alexander Chaschin <ac@happynumbers.com>
2023-06-18 00:47:56 +0300

    add buttons

--- dialogue_scene.gd
+++ dialogue_scene.gd
@@ -1,14 +1,17 @@
 extends DialogueInterpreter
 
-@export_node_path("Label") var _name
-@onready var nameLabel = get_node(_name) as Label
+@export_node_path("Label") var _name_label
+@onready var name_label = get_node(_name_label) as Label
 
-@export_node_path("Label") var _text
-@onready var textLabel = get_node(_text) as Label
+@export_node_path("Label") var _text_label
+@onready var text_label = get_node(_text_label) as Label
 
 func _ready():
   run("start")
 
 func _say(name_t: String, text: String):
-  nameLabel.text = name_t
-  textLabel.text = text
+  name_label.text = name_t
+  text_label.text = text
+
+func _ask(_text: String, _options: Array[String]) -> int:
+  return -1

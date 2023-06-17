f3b7bc197ef7aced59c17b753674ebef6175dee9
Alexander Chaschin <ac@happynumbers.com>
2023-06-18 01:05:58 +0300

    add question logic

--- dialogue_scene.gd
+++ dialogue_scene.gd
@@ -6,12 +6,31 @@ extends DialogueInterpreter
 @export_node_path("Label") var _text_label
 @onready var text_label = get_node(_text_label) as Label
 
+@export var button_group: ButtonGroup
+@onready var buttons = button_group.get_buttons() as Array[BaseButton]
+
 func _ready():
-  run("start")
+  _hide_buttons()  
+  await run("start")
 
 func _say(name_t: String, text: String):
   name_label.text = name_t
   text_label.text = text
 
-func _ask(_text: String, _options: Array[String]) -> int:
-  return -1
+func _ask(text: String, options: Array[String]) -> int:
+  _show_buttons(options)
+  await button_group.pressed
+  _hide_buttons()  
+  var pressed = button_group.get_pressed_button().text
+  var idx = options.find(pressed)
+  return idx
+  
+func _show_buttons(options: Array[String]):
+  for i in buttons.size():
+    if i == options.size(): return
+    buttons[i].visible = true
+    buttons[i].text = options[i]
+
+func _hide_buttons():
+  for button in buttons:
+    button.visible = false

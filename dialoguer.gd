8adc5d94e1c1854d15f0f5eccfe0674c0f49dc99
Alexander Chaschin <ac@happynumbers.com>
2023-06-17 15:46:54 +0300

    add ui paths

--- dialoguer.gd
+++ dialoguer.gd
@@ -2,5 +2,16 @@ extends Node
 
 @export var dialogue: DialogueData
 
+@export_node_path("Label") var _whom
+@onready var whom = get_node(_whom)
+
+@export_node_path("Label") var _text
+@onready var text = get_node(_text)
+
 func _ready():
   dialogue.to_ast()
+  await get_tree().create_timer(1).timeout
+  say()
+  
+func say():
+  print(whom)

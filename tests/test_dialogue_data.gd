9f037fd202247af7a9ec412fded452c78d31d1fe
Alexander Chaschin <ac@happynumbers.com>
2023-06-18 16:25:59 +0300

    add multiline + code data

--- tests/test_dialogue_data.gd
+++ tests/test_dialogue_data.gd
@@ -16,6 +16,14 @@ class TestPhrase:
       d.parse()["start"],
       [{ "type": "phrase", "name": "John", "text": "Hey!\nAre we gonna dance?" }]
     )    
+    
+  func test_multiline_with_next():
+    var d = DialogueData.new("#start\nJohn: \nHey!\nAre we gonna dance?\n$ pause(1000)")
+    assert_eq_deep(
+      d.parse()["start"],
+      [{ "type": "phrase", "name": "John", "text": "Hey!\nAre we gonna dance?" },
+       { "type": "code", "code": "pause(1000)"}]
+    )        
 
   func test_special_symbol():
     var d = DialogueData.new("#start\n\\$John: Hey!")

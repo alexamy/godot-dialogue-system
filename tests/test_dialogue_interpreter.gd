ca19dd69cfa28099b92278a9a52a179f2813845e
Alexander Chaschin <ac@happynumbers.com>
2023-06-18 23:43:29 +0300

    extract make interpreter

--- tests/test_dialogue_interpreter.gd
+++ tests/test_dialogue_interpreter.gd
@@ -4,13 +4,17 @@ extends GutTest
 class TestPhrase:
   extends GutTest
   
-  func test_variable_interpolation():
+  func make_interpreter(dialogue):
     set_double_strategy(DOUBLE_STRATEGY.SCRIPT_ONLY)
-    var dialogue = DialogueData.new("#start\nJohn: Hey {wife}!")
     var interpreter_path = load("res://tests/test_interpreter.gd")
     var interpreter = partial_double(interpreter_path).new()
     interpreter.dialogue = dialogue
-    add_child_autofree(interpreter)
+    add_child_autofree(interpreter)  
+    return interpreter
+  
+  func test_variable_interpolation():
+    var dialogue = DialogueData.new("#start\nJohn: Hey {wife}!")
+    var interpreter = make_interpreter(dialogue)
     interpreter.run_block("start")
     assert_called(interpreter, '_say', ['John', 'Hey Mary!'])
     

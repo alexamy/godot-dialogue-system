d165bba95b3ced7d3a835d72e8370cc925b2f5f0
Alexander Chaschin <ac@happynumbers.com>
2023-06-20 23:20:46 +0300

    add anchor and base

---
+++ addons/godialosys/nodes/node_anchor.gd
@@ -0,0 +1,6 @@
+extends DialogueBaseNode
+
+class_name DialogueAnchorNode
+
+func _on_resize_request(new_minsize):
+  size.x = new_minsize.x
---
+++ addons/godialosys/nodes/node_base.gd
@@ -0,0 +1,9 @@
+extends GraphNode
+
+class_name DialogueBaseNode
+
+func _ready():
+  connect("resize_request", _on_resize_request)
+
+func _on_resize_request(new_minsize):
+  size = new_minsize

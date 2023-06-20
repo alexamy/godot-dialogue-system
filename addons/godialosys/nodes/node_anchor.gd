35dd9f88d4a53e506290a210eb50a92746323116
Alexander Chaschin <ac@happynumbers.com>
2023-06-21 00:40:12 +0300

    add anchor

--- addons/godialosys/nodes/node_anchor.gd
+++ addons/godialosys/nodes/node_anchor.gd
@@ -6,3 +6,10 @@ class_name DialogueAnchorNode
 
 func _on_resize_request(new_minsize):
   size.x = new_minsize.x
+
+# TODO turn anchor into object key
+func as_object():
+  return {
+    "type": "achor",
+    "name": $Name.text,
+  }

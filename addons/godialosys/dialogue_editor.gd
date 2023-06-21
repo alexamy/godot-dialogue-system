e06cd07f3356d01dff52504bf834d6fcc1f92cb7
Alexander Chaschin <ac@happynumbers.com>
2023-06-21 07:58:07 +0300

    add callback

--- addons/godialosys/dialogue_editor.gd
+++ addons/godialosys/dialogue_editor.gd
@@ -9,6 +9,8 @@ func _ready():
 func _on_graph_edit_connection_request(from_node, from_port, to_node, to_port):
   print(from_node, from_port, to_node, to_port)
 
-
 func _on_graph_edit_connection_to_empty(from_node, from_port, release_position):
-  print(from_node, from_port, release_position)
+  pass
+
+func _on_graph_edit_popup_request(position):
+  pass # Replace with function body.

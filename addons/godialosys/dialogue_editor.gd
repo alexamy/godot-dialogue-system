98d6538f8503dc77ff275aa6efc7274c5a8aa29c
Alexander Chaschin <ac@happynumbers.com>
2023-06-21 08:56:56 +0300

    add disconnects

--- addons/godialosys/dialogue_editor.gd
+++ addons/godialosys/dialogue_editor.gd
@@ -1,16 +1,15 @@
 @tool
 
-extends Control
+extends GraphEdit
 
 func _ready():
   pass
 
+func _on_connection_request(from_node, from_port, to_node, to_port):
+  var node = get_node(str(from_node))
+  connect_node(from_node, from_port, to_node, to_port)
+  if node is DialogueAnchorNode:
+    pass
 
-func _on_graph_edit_connection_request(from_node, from_port, to_node, to_port):
-  print(from_node, from_port, to_node, to_port)
-
-func _on_graph_edit_connection_to_empty(from_node, from_port, release_position):
-  pass
-
-func _on_graph_edit_popup_request(position):
-  pass # Replace with function body.
+func _on_disconnection_request(from_node, from_port, to_node, to_port):
+  disconnect_node(from_node, from_port, to_node, to_port)
